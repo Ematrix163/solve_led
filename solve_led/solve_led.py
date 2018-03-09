5bba6151b539b53166696dcd19e086cd04ea5e30
Ematrix163 <milkbar1994@hotmail.com>
2018-03-09 00:23:00 +0000

    Add the main function and use TDD to test each one.

--- solve_led/solve_led.py
+++ solve_led/solve_led.py
@@ -10,10 +10,10 @@
 
 # import time to count the running time
 import time 
-
+import os
 # I use numpy package here because it was written in C and its operations on array is very fast
 import numpy as np
-import os
+
 import sys
 import getopt
 import requests
@@ -23,21 +23,78 @@ def get_net_data(url):
 	'''
 	This function is to get network data and return it.
 	'''
-
-	content = requests.get(url).text
-	return content
+	try:
+		content = requests.get(url).text
+		return content
+	except:
+		print('Cannot find the url! Please check!')
+		return False
 
 
 def get_local_data(path):
 	'''
 	This file is to get the local file data and return it
 	'''
-	if os.path.exists(path):
+	if (os.path.exists(path)):
 		with open(path) as f:
 			content = f.read()
 			return content
 	else:
+		print('Cannot find the file! Please check!')
 		return False
+
+def execute(content):
+	'''
+	This is the main function of the program. 
+	'''
+	content = content.replace('\n',';').split(';')
+	lines = int(content[0])
+
+	#  initialise numpy array, the size is lines * lines, type is bool
+	led = np.zeros((lines,lines),dtype = bool)
+
+	# traverse each line of the instructions
+	for each in content[1:-1]:
+
+		# use valid function to check whether this instruction is valid
+		results = valid(each)
+
+		# if it is valid
+		if results:
+			x1 = int(results[1])
+			y1 = int(results[2])
+			x2 = int(results[3])
+			y2 = int(results[4])
+
+			# Here we have to consider about the the point may goes from higher to lower or lower to higher
+			# so we have to judege the situation and handle it
+			if (x1 > x2):
+				x1, x2 = x2, x1
+			if (y1 > y2):
+				y1, y2 = y2, y1
+
+			# sometiems there may be some negative numbers, we have to assign 0 to them
+			if (x2 > 0 and y2 > 0 and (x1 < 0 or y1 < 0)):
+				x1 = max(0, x1)
+				y1 = max(0, y1)
+
+			if (results[0] == 'turn on'):
+				# The range of every point is [0,lines-1]
+				led[x1:x2+1, y1:y2+1] = True
+
+			elif (results[0] == 'turn off'):
+				# turn_on(x1, y1, x2, y2)
+				led[x1:x2+1, y1:y2+1] = False
+
+			elif results[0] == 'switch':
+				led[x1:x2+1, y1:y2+1] = np.invert(led[x1:x2+1, y1:y2+1])
+
+
+	outcome = led.sum()
+	# Print the outcome
+	print('%d lights are on after executing the instructions.' % outcome)
+	return outcome
+
 
 
 def valid(input):
@@ -55,3 +112,42 @@ def valid(input):
 		return results[0]
 	else:
 		return False
+
+def main():
+	'''
+	This is the main entrance of the program.
+	'''
+	start_time = time.time()
+	
+	try:
+		options,args = getopt.getopt(sys.argv[1:],"in:", ["input="])
+		for name,value in options:
+			if name in ("-in","--input"):
+				choose(value)
+	except getopt.GetoptError:
+		sys.exit()
+
+	# print the running time
+	print("----- The running time: %.2f seconds -----" % (time.time() - start_time))
+
+
+def choose(path):
+	'''
+	The function is to judge whether a given path is a local path or network url
+	'''
+
+	if re.match(r'^http|ftp|https', path):
+		# get network data
+		content = get_net_data(path)
+		if (content):
+			execute(content)
+	else:
+		# get local file data
+		content = get_local_data(path)
+		if(content):
+			execute(content)
+	
+
+
+if __name__ == "__main__":
+	main()
--- tests/test_solve_led.py
+++ tests/test_solve_led.py
@@ -3,34 +3,62 @@
 
 """Tests for `solve_led` package."""
 
-
+# import test module
 import pytest
 
+# import solve_led module
 from solve_led import solve_led
 
 
 def test_get_local_data():
 
+	# This is to test when the file doesn't exsist, the function should return false
 	path = './tests/no_file'
 	assert solve_led.get_local_data(path) == False
+
+	# This is to test when the file exsists, the function should return its content
 	path = './tests/test_file.txt'
 	content = 'switch 109,360 through 331,987'
 	assert solve_led.get_local_data(path) == content
 
 
 def test_get_net_data():
+
+	# This is to test whether the corrcet url work
 	url = 'http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_d.txt'
 	assert solve_led.get_net_data(url).replace('\n',';').split(';')[0] == '1000'
 	assert solve_led.get_net_data(url).replace('\n',';').split(';')[1] == 'switch 109,360 through 331,987'
 	assert solve_led.get_net_data(url).replace('\n',';').split(';')[-2] == 'turn off 81,52 through 255,768'
 
+	# This is to test when input an fake url, the get_net_data should return false
+	fake_url = 'http://www.claritytrfgfdfdsfdsd.txt'
+	assert solve_led.get_net_data(fake_url) == False
+
 def test_valid():
 	# This is corrct input 
+
+	# When the input starts with turn on 
 	input = 'turn on 5,5 through 10,10'
 	assert solve_led.valid(input) == ('turn on', '5', '5', '10', '10')
 
+	# When the input starts with switch 
 	input = 'switch 5,5 through 10,10'
 	assert solve_led.valid(input) == ('switch', '5', '5', '10', '10')
 
+	# When the input starts with turn off
 	input = 'turn off 5,5 through 10,10'
 	assert solve_led.valid(input) == ('turn off', '5', '5', '10', '10')
+
+
+def test_execute():
+
+	# This is to test when If a command affects a region outside the area of the grid, 
+	# then it will still be executed, but only on the region of lights inside the boundary of the grid.
+	content = '10 \n turn on 5,5 through 5,5 \n switch 100,100 through 1099,1990 \n'
+	assert solve_led.execute(content) == 1
+
+
+	# If there is some negative in the input, the function should handle correctly
+	content = '10 \n switch 5,5 through 12,13 \n switch 12,15 through 18,19 \n switch -1,-100 through 3,5 \n switch 5,15 through 6,11 \n'
+	assert solve_led.execute(content) == 49
+
